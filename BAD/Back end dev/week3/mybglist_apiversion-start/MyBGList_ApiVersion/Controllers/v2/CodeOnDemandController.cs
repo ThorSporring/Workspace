8f7d8d9b76167f83f667be5a3c09afc7365dcbc6
Thor Sporring <au689912@uni.au.dk>
2024-02-12 14:46:54 +0100

    ex3 completed

---
+++ BAD/Back end dev/week3/mybglist_apiversion-start/MyBGList_ApiVersion/Controllers/v2/CodeOnDemandController.cs
@@ -0,0 +1,62 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBGList.Models;
+using MyBGList_ApiVersion.DTO.v2;
+
+namespace MyBGList.Controllers.v2
+{
+    [Route("v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    public class CodeOnDemandController : ControllerBase
+    {
+
+        [HttpGet("Test")]
+        public ContentResult Test()
+        {
+            var content = "<script>" +
+        "window.alert('Your client supports JavaScript!" +
+        "\\r\\n\\r\\n" +
+        $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+        "\\r\\n" +
+        "Client time (UTC): ' + new Date().toISOString());" +
+        "</script>" +
+        "<noscript>Your client does not support JavaScript</noscript>";
+        
+            return Content(content ,"text/html");
+        }
+
+        [HttpGet("Test2/{addMinutes}")]
+        public ContentResult Test2(int? addMinutes)
+        {
+            var content = "";
+            if(addMinutes.HasValue)
+             {
+                content = "<script>" +
+                "window.alert('Your client supports JavaScript!" +
+                "\\r\\n\\r\\n" +
+                $"Server time (UTC): {DateTime.UtcNow.AddMinutes((double)addMinutes).ToString("o")}" +
+                "\\r\\n" +
+                $"Client time (UTC): ' + new Date().toISOString());" +
+                "</script>" +
+                "<noscript>Your client does not support JavaScript</noscript>";
+             }
+             else{
+                content = "<script>" +
+                "window.alert('Your client supports JavaScript!" +
+                "\\r\\n\\r\\n" +
+                $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+                "\\r\\n" +
+                "Client time (UTC): ' + new Date().toISOString());" +
+                "</script>" +
+                "<noscript>Your client does not support JavaScript</noscript>";
+             }
+
+            
+
+            return Content(content, "text/html");
+
+        }
+    }
+}
